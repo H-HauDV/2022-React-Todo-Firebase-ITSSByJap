c2afe9076173e86fda1d58a8e52a70369ccb930f
DoanHau <>
2022-06-21 06:06:49 +0000

    add todo to firebase

--- src/hooks/FBStorage.js
+++ src/hooks/FBStorage.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 
 /* ライブラリ */
 import { addFirebaseItem, updateFirebaseItem, getFirebaseItems, clearFirebaseItem } from "../lib/firebase";
@@ -19,5 +20,27 @@ function FBStorage() {
     await addFirebaseItem(newItem);
     setItems([...items, newItem]);
   };
+
+  const updateItem = async checked => {
+    const changes = { done: !checked.done };
+    await updateFirebaseItem(changes, checked.id);
+    const newItems = items.map((item) => {
+      if (item.id === checked.id) {
+        item = { ...item, changes}
+      }
+      return item;
+    })
+    setItems(newItems);
+  }
+
+  const clearItems = () => {
+    items.map(item => {
+      clearFirebaseItem(item);
+    })
+    setItems([]);
+  };
+
+  return [items, addItem, updateItem, clearItems];
 }
+
 export default FBStorage;
